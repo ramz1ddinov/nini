0533c04f89013b78929c2f00fb6eddb22c876764
ramz1ddinov <tsahribonu@gmail.com>
2025-11-03 09:04:26 +0500

    Add files via upload

---
+++ Новая папка (2)/script.js
@@ -0,0 +1,30 @@
+let text = 'Hello dilshod'
+let text2 = 'dilishgoda'
+let text3 = `Xunuk ${text}`
+
+
+let str = "hello world!"
+console.log(str.trim());
+
+
+
+let a = "Saidaakbar";
+let b = "turshak";
+let result = a.contact(b)
+console.log(result);
+
+
+let textin = "I love JvaScript"
+console.log(textIn.includes("love"));
+console.log(textIn.includes("phthon"));
+
+
+
+let name = "abdulaziz";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+
+let textRep = "I like JavaScrpit";
+let newText = textRep.replace("JavaScript", "Pythone");
+console.log(newText);
